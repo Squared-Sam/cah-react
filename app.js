a4c4c9f11dc3c1bb69733a9b64044676c858dced
Sam Mercer <sam@mercerfamily.org.uk>
2020-02-23 13:45:04 +0000

    Move everything behind api

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var gameRouter = require('./routes/game');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const gameRouter = require('./routes/game');
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('api/', indexRouter);
 app.use('api/users', usersRouter);
-app.use('api/game',gameRouter);
+app.use('api/game', gameRouter);
+
 module.exports = app;
