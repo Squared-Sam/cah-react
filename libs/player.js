0376bfefff0af79de866635584f986ba15018b27
Sam <Lagicrus@users.noreply.github.com>
2020-02-24 23:52:05 +0000

    Libs as well

--- libs/player.js
+++ libs/player.js
@@ -1,24 +1,27 @@
-const uuidv4 = require('uuid/v4');
-
+const uuidv4 = require("uuid/v4");
 
 class Player {
-	constructor(nickname) {
-		this._id = uuidv4();
-		this._nickname = nickname;
-		this._score = 0;
-	}
-	get id() {
-		return this._id;
-	}
-	get nickname() {
-		return this._nickname;
-	}
-	get score() {
-		return this._score;
-	}
-	set score(score) {
-		this._score = score
-	}
+  constructor(nickname) {
+    this._id = uuidv4();
+    this._nickname = nickname;
+    this._score = 0;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get nickname() {
+    return this._nickname;
+  }
+
+  get score() {
+    return this._score;
+  }
+
+  set score(score) {
+    this._score = score;
+  }
 }
 
-module.exports = Player
+module.exports = Player;
