813ecc2be3a1d89bfe5772a09ae6524fda55da4a
Sam <Lagicrus@users.noreply.github.com>
2020-02-24 23:50:04 +0000

    ESLint and other fixes

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors')
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const gameRouter = require('./routes/game');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const gameRouter = require("./routes/game");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
-	extended: true
+  extended: true
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/', indexRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/game', gameRouter);
+app.use("/api/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/game", gameRouter);
 
 module.exports = app;
