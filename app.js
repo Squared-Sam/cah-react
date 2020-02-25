735ee68be7905fc2db26cc7db4590d82e0b468ec
Sam Mercer <sam@mercerfamily.org.uk>
2020-02-25 22:53:18 +0000

    Add check for correct content header

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -14,11 +13,17 @@ const app = express();
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({
-//   extended: true
-// }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  if (!req.is('application/json')) {
+    next('APIs require application/json');
+  }
+  next();
+});
 
 app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
